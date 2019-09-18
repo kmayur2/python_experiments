3bff54fb385eef8d3ae83fbfa0d86333653fada6
Mayur K <kmayur2@gmail.com>
2019-09-18 21:50:39 +0530

    experiments

--- class_experiment_1/class_test.py
+++ class_experiment_1/class_test.py
@@ -2,15 +2,27 @@
 
 class MyClass:
 	
-	mName = ""
+	mName = "abc"
 	def __init__(self, name):
-		self.mName = name;
+		self.mName = name
+	
+	def changeInstName(self, name):
+		mName = name
+		print "In changeInstName, mName: "+mName+" self.mName: "+self.mName
 	
 	def printMyName(self):
-		print "MyClass, name is "+self.mName
+		print "MyClass, name is "+self.mName 
 		
 
 if __name__ == '__main__':
 	print "In Main"
 	m = MyClass("Mayur")
 	m.printMyName()
+	m.changeInstName("stuv")
+	
+	print ("Print 1 {}".format(m.__class__.mName))
+	print ("Print 2 {}".format(m.mName))
+	
+	n = MyClass("One") 
+	print "Print 1 {}".format(n.__class__.mName)
+	print "Print 2  {}".format(n.mName)
